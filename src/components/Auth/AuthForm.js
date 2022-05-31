d33ac890a5959198f977f235516bd91b85711bbe
thang <thanglhfx12120@funix.edu.vn>
2022-05-31 17:07:44 +0700

    first commit

---
+++ src/components/Auth/AuthForm.js
@@ -0,0 +1,101 @@
+import { useState, useRef, useContext } from 'react';
+import {useHistory} from 'react-router-dom';
+import AuthContext from '../../store/auth-context';
+
+import classes from './AuthForm.module.css';
+
+const AuthForm = () => {
+  const history = useHistory();
+  const authCtx = useContext(AuthContext);
+
+  const [isLogin, setIsLogin] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+
+    setIsLoading(true);
+    let url;
+    if (isLogin) {
+      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyANKShvL8cfL5SG4Oic2k3gMzjF6rlkeNM';
+    } else {
+      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyANKShvL8cfL5SG4Oic2k3gMzjF6rlkeNM';
+    }
+
+    fetch(
+      url,
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          email: enteredEmail,
+          password: enteredPassword,
+          returnSecureToken: true
+        }),
+        headers: {
+          'Content-Type' : 'application/json'
+        }
+      }
+    ).then(res => {
+      setIsLoading(false);
+      if (res.ok) {
+        return res.json();
+      } else {
+        return res.json().then(data => {
+          let errorMessage = 'Authentication failed!';
+          // if (data && data.error && data.error.message) {
+          //   errorMessage = data.error.message;
+          // }
+          
+          throw new Error(errorMessage);
+        });
+      }
+    })
+    .then(data => {
+      const expirationTime = new Date(new Date().getTime() + (+data.expiresIn * 1000));
+      authCtx.login(data.idToken, expirationTime.toISOString());
+      history.replace('/');
+    })
+    .catch(err => {
+      alert(err.message);
+    });
+  };
+
+  const switchAuthModeHandler = () => {
+    setIsLogin((prevState) => !prevState);
+  };
+
+  return (
+    <section className={classes.auth}>
+      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
+      <form onSubmit={submitHandler}>
+        <div className={classes.control}>
+          <label htmlFor='email'>Your Email</label>
+          <input ref={emailInputRef} type='email' id='email' required />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor='password'>Your Password</label>
+          <input ref={passwordInputRef} type='password' id='password' required />
+        </div>
+        <div className={classes.actions}>
+          {!isLoading && <button>{isLogin ? 'Login' : 'Create Account'}</button>}
+          {isLoading && <p>Sending request...</p>}
+          <button
+            type='button'
+            className={classes.toggle}
+            onClick={switchAuthModeHandler}
+          >
+            {isLogin ? 'Create new account' : 'Login with existing account'}
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default AuthForm;
